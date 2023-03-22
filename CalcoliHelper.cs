4f59a21aa2b437bb57dfe950a51b4827880e811e
millooo94 <camillomusmeciunict@gmail.com>
2023-03-22 19:17:55 +0100

    Aggiungere i file di progetto.

---
+++ CalcoliHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class CalcoliHelper
+{
+    public static int IntSum(int x, int y) => x + y;
+
+    public static double DoubleSum(double x, double y) => x + y;
+
+    public static int IntDiff(int x, int y) => x - y;
+
+    public static double DoubleDiff(double x, double y) => x - y;
+
+    public static int IntMult(int x, int y) => x * y;
+
+    public static double DoubleMult(double x, double y) => x * y;
+
+    public static int IntAbs(int x)
+    {
+        if (x < 0) return x * -1;
+
+        else return x;
+    }
+
+    public static double DoubleAbs(double x)
+    {
+        if (x < 0) return x * -1;
+
+        else return x;
+    }
+
+    public static int IntMin(int x, int y)
+    {
+        if (x < y) return x;
+
+        else return y;
+    }
+
+    public static double DoubleMin(double x, double y)
+    {
+
+        if (x < y) return x;
+
+        else return y;
+
+    }
+
+    public static int IntMax(int x, int y)
+    {
+        if (x > y) return x;
+
+        else return y;
+    }
+
+    public static double DoubleMax(double x, double y)
+    {
+        if (x > y) return x;
+
+        else return y;
+    }
+
+    public static int IntPow(int @base, int exp)
+    {
+        if (@base == 0 && exp == 0) return 1;
+        else if (exp == 0) return 1;
+        else if (exp == 1) return @base;
+        else
+        {
+            var result = @base;
+            for (int i = 1; i < exp; i++)
+            {
+                result *= @base;
+            }
+            return result;
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿var num1 = 2;
+var num2 = 3;
+var num3 = -5;
+var num4 = 3.5;
+var num5 = 4.8;
+
+//SOMMA INTERI
+var sommaInteri = CalcoliHelper.IntSum(num1, num2);
+Console.WriteLine(sommaInteri);
+
+//SOMMA DOUBLE
+var sommaDouble = CalcoliHelper.DoubleSum(num4, num5);
+Console.WriteLine(sommaDouble);
+
+//DIFFERENZA INTERI
+var differenzaInteri = CalcoliHelper.IntDiff(num1, num2);
+Console.WriteLine(differenzaInteri);
+
+//DIFFERENZA DOUBLE
+var differenzaDouble = CalcoliHelper.DoubleDiff(num4, num5);
+Console.WriteLine(differenzaDouble);
+
+//MOLTIPLICAZIONE INTERI
+var moltiplicazioneInteri = CalcoliHelper.IntMult(num1, num2);
+Console.WriteLine(moltiplicazioneInteri);
+
+//MOLTIPLICAZIONE DOUBLE
+var moltiplicazioneDouble = CalcoliHelper.DoubleMult(num4, num5);
+Console.WriteLine(moltiplicazioneDouble);
+
+//VALORE ASSOLUTO INTERO
+var valoreAssolutoIntero = CalcoliHelper.IntAbs(num3);
+Console.WriteLine(valoreAssolutoIntero);
+
+//VALORE ASSOLUTO DOUBLE
+var valoreAssolutoDouble = CalcoliHelper.DoubleAbs(num3);
+Console.WriteLine(valoreAssolutoDouble);
+
+//MINIMO INTERI
+var minimoInteri = CalcoliHelper.IntMin(num1, num2);
+Console.WriteLine(minimoInteri);
+
+//MINIMO DOUBLE
+var minimoDouble = CalcoliHelper.DoubleMin(num4, num5);
+Console.WriteLine(minimoDouble);
+
+//MASSIMO INTERI
+var massimoInteri = CalcoliHelper.IntMax(num1, num2);
+Console.WriteLine(massimoInteri);
+
+//MASSIMO DOUBLE
+var massimoDouble = CalcoliHelper.DoubleMax(num4, num5);
+Console.WriteLine(massimoDouble);
+
+//POTENZA INTERO
+var potenzaIntero = CalcoliHelper.IntPow(num1, num2);
+Console.WriteLine(potenzaIntero);
